623a6c5387ce708fa81f03476e598cfcc1afa312
James Hrisho <james.hrisho@gmail.com>
2014-06-08 15:20:32 -0400

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+  source = require('vinyl-source-stream'),
+  browserify = require('browserify'),
+  connect = require('gulp-connect-multi')(),
+  $ = require('gulp-load-plugins')();
+
+
+gulp.task('default',
+  connect.server({
+    root: [__dirname],
+    port: 9002,
+    livereload: true,
+    open:{
+      browser:  'Google Chrome' //'chrome'
+    }
+}));
---
+++ slides/slide-1.md
@@ -0,0 +1,5 @@
+#ReactJS
+
+by
+
+##James Hrisho
